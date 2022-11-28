059d31cc443a1d1ee1c224008f1559bfa5cd68fe
Raveesh Nilaweera <107676259+Rstacx@users.noreply.github.com>
2022-11-28 20:42:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,6 +13,44 @@ client.on('ready', () => {
   console.log("rsn music bot ready to play music!");
 })
 
+// web server 
+
+const express = require("express");
+const app = express();
+app.use(express.static("public"));
+const path = require("path");
+app.use("/css",express.static(path.resolve(__dirname + `/css/`)));
+ const templateDir = path.resolve(__dirname + `/`);
+
+  app.locals.domain = process.env.PROJECT_DOMAIN;
+  app.engine("html", require("ejs").renderFile);
+  app.set("view engine", "html");
+
+  var bodyParser = require("body-parser");
+  app.use(bodyParser.json());
+
+  const renderTemplate = (res, req, template, data = {}) => {
+    const baseData = {
+      path: req.path
+    };
+    res.render(
+      path.resolve(`${templateDir}${path.sep}${template}`),
+      Object.assign(baseData, data)
+    );
+  };
+
+  app.get("/", (req, res) => {
+    res.send("App Running")
+  });
+  app.get("/*", (req, res) => {
+    res.send("404 Not Found")
+  });
+
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("starting " + listener.address().port);
+});
+
 const queue = new Map();
 
 queue.server_queue = null;
